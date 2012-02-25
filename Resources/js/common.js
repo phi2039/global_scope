c7fc52dcbb31b3e0e01bf0eb4890668c6393daa1
Chris Lance <chris_lance@sbcglobal.net>
2012-02-25 00:35:41 -0500

    UPDATE: More playing...

---
+++ Resources/js/common.js
@@ -0,0 +1,6 @@
+/**
+ * @author Christopher Lance
+ */
+
+//   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+google.load("jquery", "1.7.1");
---
+++ Resources/js/geo.js
@@ -0,0 +1,4 @@
+/**
+ * @author Christopher Lance
+ */
+
